015e9331512bc738debbaccfe98bed3a2b9e8120
zhaoy <1097794678@qq.com>
2020-03-21 00:43:51 +0800

    ddddddddd

--- LeetCode/l4.py
+++ LeetCode/l4.py
@@ -20,82 +20,77 @@ nums2 = [3, 4]
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
 
-
+"""
+二分查找
+总是会出现越界问题，在快要下降到边界的时候停止，采用慢速查找
+"""
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         n1s = 0
-        n1e = len(nums1) - 1
+        n1e = len(nums1)
         n2s = 0
-        n2e = len(nums2) - 1
-        gcd = (len(nums2) + len(nums1)) % 2
-        if not gcd:
-            m = (len(nums1) + len(nums2)) // 2 - 1
-        else:
-            m = (len(nums1) + len(nums2)) // 2
-        while m > 0 and nums1[n1s:n1e + 1] and nums2[n2s:n2e + 1]:
-            n1m = n1s + (n1e - n1s) // 2
-            n2m = n2s + (n2e - n2s) // 2
+        n2e = len(nums2)
+        g = (len(nums1) + len(nums2)) % 2
+        m = (len(nums1) + len(nums2)) // 2
+        if not g:
+            m -= 1
+        while n1e > n1s and n2e > n2s and m > 2:
+            n1m = n1s + (n1e - n1s) // 2 - 1
+            n2m = n2s + (n2e - n2s) // 2 - 1
             if nums1[n1m] < nums2[n2m]:
                 m -= n1m - n1s
-                if n1s != n1m:
-                    n1s = n1m
-                else:
-                    n1s += n1m + 1
-                    m -= 1
+                n1s = n1m
                 n2e = n2m
             else:
                 m -= n2m - n2s
-                if n2s != n2m:
-                    n2s = n2m
-                else:
-                    n2s += n2m + 1
-                    m -= 1
+                n2s = n2m
                 n1e = n1m
-            print(nums1[n1s:n1e + 1], nums2[n2s:n2e + 1], m)
-        print(n1s, n1e, n2s, n2e)
-        if m == 0:
-            if gcd == 0:
-                m1 = float("inf")
-                if n1s + 1 < len(nums1):
-                    m1 = (nums1[n1s] + nums1[n1s + 1]) / 2
-                m2 = float("inf")
-                if n2s + 1 < len(nums2):
-                    m2 = (nums2[n2s] + nums2[n2s + 1]) / 2
-                m3 = float("inf")
-                if n2s < len(nums2) and n1s < len(nums1):
-                    m3 = (nums1[n1s] + nums2[n2s]) / 2
-                return float('%.1f' % min(m1, m2, m3))
+        for _ in range(0, m):
+            n1 = float('inf')
+            n2 = float('inf')
+            if n1s < len(nums1):
+                n1 = nums1[n1s]
+            if n2s < len(nums2):
+                n2 = nums2[n2s]
+            if n1 > n2:
+                n2s += 1
             else:
-                m1 = float("inf")
-                if n1s < len(nums1):
-                    m1 = nums1[n1s]
-                m2 = float("inf")
-                if n2s < len(nums2):
-                    m2 = nums2[n2s]
-                return float('%.1f' % min(m1, m2))
+                n1s += 1
+        print(n1s, n2s, m)
+        if not g:
+            n1 = float('inf')
+            n2 = float('inf')
+            n3 = float('inf')
+            n4 = float('inf')
+            if n1s < len(nums1):
+                n1 = nums1[n1s]
+            if n2s < len(nums2):
+                n2 = nums2[n2s]
+            if n1s + 1 < len(nums1):
+                n3 = nums1[n1s + 1]
+            if n2s + 1 < len(nums2):
+                n4 = nums2[n2s + 1]
+            return float('%.1f' % (min(n1 + n2, n1 + n3, n2 + n4) / 2))
         else:
-            if not nums1[n1s:n1e + 1]:
-                if gcd == 0:
-                    return float('%.1f' % ((nums2[n2s + m] + nums2[n2s + m + 1]) / 2))
-                else:
-                    return float('%.1f' % nums2[n2s + m])
-            elif not nums2[n2s:n2e + 1]:
-                if gcd == 0:
-                    return float('%.1f' % ((nums1[n1s + m] + nums1[n1s + m + 1]) / 2))
-                else:
-                    return float('%.1f' % nums1[n1s + m])
+            n1 = float('inf')
+            n2 = float('inf')
+            if n1s < len(nums1):
+                n1 = nums1[n1s]
+            if n2s < len(nums2):
+                n2 = nums2[n2s]
+            return float('%.1f' % min(n1, n2))
 
 
 if __name__ == '__main__':
     # 1,2,2,3,3,4,5,8,9,10
-    nums1 = [2, 3, 5, 8, 9, 10]
-    nums2 = [1, 2, 3, 4]
-    print(Solution().findMedianSortedArrays(nums1, nums2))
-    # #
-    # nums1 = [1, 2, 3, 5, 5, 6]
-    # nums2 = [3, 4, 8, 12, 14]
-    # # 1,2,3,3,4,5,5,6,8,12,14
+    # nums1 = [2, 3, 5, 8, 9, 10]
+    # nums2 = [1, 2, 3, 4]
     # print(Solution().findMedianSortedArrays(nums1, nums2))
+    # #
+    nums1 = [1, 2, 3, 5, 5, 6]
+    nums2 = [3, 4, 8, 12, 14]
+    # 1,2,3,3,4,5,5,6,8,12,14
+    print(Solution().findMedianSortedArrays(nums1, nums2))  # 5
     # nums1 = [1, 3]
     # nums2 = [2]
     # # 1,2,3
@@ -112,7 +107,13 @@ if __name__ == '__main__':
     # nums2 = [1, 2, 3]
     # # 1,1,2,2,2,3
     # print(Solution().findMedianSortedArrays(nums1, nums2))
-    # nums1 = [2]
-    # nums2 = [1, 3, 4]
-    # # 1,1,2,2,2,3
+    nums1 = [2]
+    nums2 = [1, 3, 4]
+    # 1,1,2,2,2,3
+    print(Solution().findMedianSortedArrays(nums1, nums2))
+    nums1 = [1, 2]
+    nums2 = [-1, 3]
+    print(Solution().findMedianSortedArrays(nums1, nums2))
+    # nums1 = [1, 6]
+    # nums2 = [2, 3, 4, 5, 7, 8]
     # print(Solution().findMedianSortedArrays(nums1, nums2))
